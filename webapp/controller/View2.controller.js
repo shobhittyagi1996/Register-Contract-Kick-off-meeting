2cedaaa5bad22d9a980663de7f4225aa2995ea16
Abhishek pal <pala@terralink-global.com>
2024-08-13 05:08:32 +0000

    updates

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
         "sap/ui/core/mvc/Controller",
         "sap/ui/model/json/JSONModel",
         "sap/ui/core/Fragment",
+        "com/sap/registercontract/controller/Utils",
+        "sap/m/ColumnListItem",
     ],
-    function (BaseController, JSONModel,Fragment) {
+    function (BaseController, JSONModel, Fragment, Utils, ColumnListItem) {
         "use strict";
 
         return BaseController.extend("com.sap.registercontract.controller.View2", {
@@ -43,13 +45,49 @@ sap.ui.define(
                 this.getView().setModel(atablemodel1, "myModel1")
 
 
+                let adata2 = [
+                    {
+                        "name": "Danara Kalenova",
+                        "email": "xyz@gmail.com"
+
+                    },
+
+
+                ];
+                let atablemodel2 = new JSONModel(adata2);
+                this.getView().setModel(atablemodel2, "myModel2")
+
+
+                let agendaData = [
+                    {
+                        "agenda": "Safety Moment",
+                        "icon": "sap-icon://ppt-attachment"
+
+                    },
+                    {
+                        "agenda": "Form of contract",
+                        "icon": "sap-icon://pdf-attachment"
+
+                    },
+                    {
+                        "agenda": "Compensation and payment",
+                        "icon": "sap-icon://ppt-attachment"
+
+                    },
+
+
+                ];
+                let agendaMdel = new JSONModel(agendaData);
+                this.getView().setModel(agendaMdel, "agendaMdel")
+
+
 
 
 
                 let logModel = new JSONModel([{
-                    " NO.": "",
+                    "NO": "1",
                     "TOPICSDISCUSSED": "",
-                    "KPOAttendeesComments": ".",
+                    "KPOAttendeesComments": "",
                     "SupplierComments": "",
 
                 },
@@ -75,11 +113,43 @@ sap.ui.define(
                 {
                     "cNo": "AP/X/24/2524",
                 },
-               ,
+                    ,
                 ]
 
                 let CNModel = new JSONModel(ContractNo);
                 this.getView().setModel(CNModel, "con")
+
+
+                var today = new Date();
+                var tomorrow = new Date();
+                tomorrow.setDate(today.getDate() + 1);
+
+                function getNextWeekday(date) {
+                    var day = date.getDay();
+                    if (day === 6) {
+                        date.setDate(date.getDate() + 2); 
+                    } else if (day === 0) {
+                        date.setDate(date.getDate() + 1); 
+                    }
+                    return date;
+                }
+
+                var oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "yyyy-MM-dd" });
+
+                var aWeekDates = [];
+                for (var i = 0; i < 5; i++) {
+                    var postDate = getNextWeekday(tomorrow);
+                    aWeekDates.push(oDateFormat.format(postDate));
+                    tomorrow = new Date(postDate);
+                    tomorrow.setDate(tomorrow.getDate() + 1);
+                }
+
+                this.getView().byId("datePicker1").setValue(aWeekDates[0]);
+                this.getView().byId("datePicker2").setValue(aWeekDates[1]);
+                this.getView().byId("datePicker3").setValue(aWeekDates[2]);
+                this.getView().byId("datePicker4").setValue(aWeekDates[3]);
+                this.getView().byId("datePicker5").setValue(aWeekDates[4]);
+
 
 
             },
@@ -89,15 +159,21 @@ sap.ui.define(
             },
 
             onAddRow: function () {
+                debugger
                 var oModel = this.getView().getModel("logModel");
                 var aItems = oModel.getData();
 
+
+
+                var nextNumber = aItems.length + 1;
+
                 aItems.push({
-                    " NO.": "",
+                    "NO": nextNumber,
                     "TOPICSDISCUSSED": "",
-                    "KPOAttendeesComments": ".",
+                    "KPOAttendeesComments": "",
                     "SupplierComments": "",
                 });
+                console.log(aItems)
                 oModel.setData(aItems);
                 oModel.refresh();
             },
@@ -145,7 +221,103 @@ sap.ui.define(
             },
             onCancelF4ExistingContractNum: function () {
                 this._F4EcnDialog.close();
+            },
+
+            moveToAvailableProductsTable: function () {
+                var oSelectedProductsTable = Utils.getSelectedProductsTable(this);
+                debugger
+                Utils.getSelectedItemContext(oSelectedProductsTable, function (oSelectedItemContext, iSelectedItemIndex) {
+                    // reset the rank property and update the model to refresh the bindings
+                    var oProductsModel = oSelectedProductsTable.getModel();
+                    oProductsModel.setProperty("Rank", Utils.ranking.Initial, oSelectedItemContext);
+
+                    // select the previously selected position
+                    var aItemsOfSelectedProductsTable = oSelectedProductsTable.getItems();
+                    var oPrevItem = aItemsOfSelectedProductsTable[Math.min(iSelectedItemIndex, aItemsOfSelectedProductsTable.length - 1)];
+                    if (oPrevItem) {
+                        oPrevItem.setSelected(true);
+                    }
+                });
+            },
+
+            onDropSelectedProductsTable: function (oEvent) {
+                var oDraggedItem = oEvent.getParameter("draggedControl");
+                var oDraggedItemContext = oDraggedItem.getBindingContext();
+                if (!oDraggedItemContext) {
+                    return;
+                }
+
+                var oRanking = Utils.ranking;
+                var iNewRank = oRanking.Default;
+                var oDroppedItem = oEvent.getParameter("droppedControl");
+
+                if (oDroppedItem instanceof ColumnListItem) {
+                    // get the dropped row data
+                    var sDropPosition = oEvent.getParameter("dropPosition");
+                    var oDroppedItemContext = oDroppedItem.getBindingContext();
+                    var iDroppedItemRank = oDroppedItemContext.getProperty("Rank");
+                    var oDroppedTable = oDroppedItem.getParent();
+                    var iDroppedItemIndex = oDroppedTable.indexOfItem(oDroppedItem);
+
+                    // find the new index of the dragged row depending on the drop position
+                    var iNewItemIndex = iDroppedItemIndex + (sDropPosition === "After" ? 1 : -1);
+                    var oNewItem = oDroppedTable.getItems()[iNewItemIndex];
+                    if (!oNewItem) {
+                        // dropped before the first row or after the last row
+                        iNewRank = oRanking[sDropPosition](iDroppedItemRank);
+                    } else {
+                        // dropped between first and the last row
+                        var oNewItemContext = oNewItem.getBindingContext();
+                        iNewRank = oRanking.Between(iDroppedItemRank, oNewItemContext.getProperty("Rank"));
+                    }
+                }
+
+                // set the rank property and update the model to refresh the bindings
+                var oSelectedProductsTable = Utils.getSelectedProductsTable(this);
+                var oProductsModel = oSelectedProductsTable.getModel();
+                oProductsModel.setProperty("Rank", iNewRank, oDraggedItemContext);
+            },
+
+            moveSelectedItem: function (sDirection) {
+                var oSelectedProductsTable = Utils.getSelectedProductsTable(this);
+                Utils.getSelectedItemContext(oSelectedProductsTable, function (oSelectedItemContext, iSelectedItemIndex) {
+                    var iSiblingItemIndex = iSelectedItemIndex + (sDirection === "Up" ? -1 : 1);
+                    var oSiblingItem = oSelectedProductsTable.getItems()[iSiblingItemIndex];
+                    var oSiblingItemContext = oSiblingItem.getBindingContext();
+                    if (!oSiblingItemContext) {
+                        return;
+                    }
+
+                    // swap the selected and the siblings rank
+                    var oProductsModel = oSelectedProductsTable.getModel();
+                    var iSiblingItemRank = oSiblingItemContext.getProperty("Rank");
+                    var iSelectedItemRank = oSelectedItemContext.getProperty("Rank");
+
+                    oProductsModel.setProperty("Rank", iSiblingItemRank, oSelectedItemContext);
+                    oProductsModel.setProperty("Rank", iSelectedItemRank, oSiblingItemContext);
+
+                    // after move select the sibling
+                    oSelectedProductsTable.getItems()[iSiblingItemIndex].setSelected(true).focus();
+                });
+            },
+
+            moveUp: function (oEvent) {
+                this.moveSelectedItem("Up");
+                oEvent.getSource().focus();
+            },
+
+            moveDown: function (oEvent) {
+                this.moveSelectedItem("Down");
+                oEvent.getSource().focus();
+            },
+
+            onBeforeOpenContextMenu: function (oEvent) {
+                oEvent.getParameters().listItem.setSelected(true);
             }
         });
-    }
+    },
+
+
+
+
 );
